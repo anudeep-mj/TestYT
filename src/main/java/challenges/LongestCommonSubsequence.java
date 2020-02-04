e32c8e90c1b0caa4a1f7ae224d99e819117db65b
anudeep-mj <anudeep.mj@gmail.com>
2020-02-03 21:41:57 -0800

    src/main/java/challenges/LongestCommonSubsequence.java

---
+++ src/main/java/challenges/LongestCommonSubsequence.java
@@ -0,0 +1,32 @@
+package challenges;
+
+/**
+ * Given two strings text1 and text2, return the length of their longest common subsequence.
+ * <p>
+ * A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted
+ * without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not).
+ * A common subsequence of two strings is a subsequence that is common to both strings.
+ */
+public class LongestCommonSubsequence {
+    public int largestCommonSubsequence(String input1, String input2) {
+        int input1Sz = input1.length();
+        int input2Sz = input2.length();
+
+        int[][] mat = new int[input1Sz + 1][input2Sz + 1];
+        for (int i = 1; i < input1Sz; i++) {
+            for (int j = 1; j < input2Sz; j++) {
+                if (input1.charAt(i) == input2.charAt(j)) {
+                    mat[i][j] = mat[i - 1][j - 1] + 1;
+                } else {
+                    mat[i][j] = Math.min(mat[i - 1][j], mat[i][j - 1]);
+                }
+            }
+        }
+        return mat[input1Sz][input2Sz];
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
