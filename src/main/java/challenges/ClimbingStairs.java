5427ee65d4575aa8d9fac72b9c7bd13a4f9a6134
anudeep-mj <anudeep.mj@gmail.com>
2020-04-16 23:47:17 -0600

    challege

---
+++ src/main/java/challenges/ClimbingStairs.java
@@ -0,0 +1,58 @@
+package challenges;
+
+/**
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * Note: Given n will be a positive integer.
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ * Watch: https://www.youtube.com/watch?v=NFJ3m9a1oJQ
+ */
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+
+        return climbStairs(n - 2) + climbStairs(n - 1);
+    }
+
+    public int climbStairsMemo(int n) {
+        int[] cache = new int[n];
+        return climbStairsHelper(cache, n, 0);
+    }
+
+    private int climbStairsHelper(int[] cache, int n, int i) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+
+        if (cache[i] > 0) {
+            return cache[i];
+        }
+
+        return climbStairsHelper(cache, n, i + 1) + climbStairsHelper(cache, n, i + 2);
+    }
+}
