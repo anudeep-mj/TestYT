5e7ef278ee22e1abc1d9bbe99a405b30ca01b9f0
Anudeep <anudeep.matta.jayaram@sap.com>
2020-04-27 15:44:39 -0700

    fix

--- src/main/java/challenges/LongestCommonSubsequence.java
+++ src/main/java/challenges/LongestCommonSubsequence.java
@@ -8,17 +8,17 @@ package challenges;
  * A common subsequence of two strings is a subsequence that is common to both strings.
  */
 public class LongestCommonSubsequence {
-    public int largestCommonSubsequence(String input1, String input2) {
+    public static int largestCommonSubsequence(String input1, String input2) {
         int input1Sz = input1.length();
         int input2Sz = input2.length();
 
         int[][] mat = new int[input1Sz + 1][input2Sz + 1];
-        for (int i = 1; i < input1Sz; i++) {
-            for (int j = 1; j < input2Sz; j++) {
-                if (input1.charAt(i) == input2.charAt(j)) {
+        for (int i = 1; i <= input1Sz; i++) {
+            for (int j = 1; j <= input2Sz; j++) {
+                if (input1.charAt(i-1) == input2.charAt(j-1)) {
                     mat[i][j] = mat[i - 1][j - 1] + 1;
                 } else {
-                    mat[i][j] = Math.min(mat[i - 1][j], mat[i][j - 1]);
+                    mat[i][j] = Math.max(mat[i - 1][j], mat[i][j - 1]);
                 }
             }
         }
@@ -26,7 +26,7 @@ public class LongestCommonSubsequence {
     }
 
     public static void main(String[] args) {
-
+        largestCommonSubsequence("abcde", "ace");
     }
 
 }
