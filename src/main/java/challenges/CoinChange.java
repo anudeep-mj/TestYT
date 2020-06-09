970420a883a0ae1c98554878457ca414865e40cb
Anudeep <anudeep.matta.jayaram@sap.com>
2020-06-09 10:10:03 -0700

    another coin change soln

--- src/main/java/challenges/CoinChange.java
+++ src/main/java/challenges/CoinChange.java
@@ -22,6 +22,25 @@ public class CoinChange
         int[] dp = new int[amount + 1];
         Arrays.sort(coins);
         for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+        }
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public int coinChange2 (int[] coins, int amount)
+    {
+        int[] dp = new int[amount + 1];
+        Arrays.sort(coins);
+        for (int i = 1; i <= amount; i++) {
             dp[i] = Integer.MAX_VALUE;
         }
 
